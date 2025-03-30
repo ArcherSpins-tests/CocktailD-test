4ff09a91524db06bc9912dc7e30609626c1cedae
kuroi-kurlik <kuroi-kurlik@proton.me>
2025-03-30 22:42:57 +0900

    Fixing tests

--- src/components/DrinkCard/DrinkCard.test.tsx
+++ src/components/DrinkCard/DrinkCard.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import DrinkCard from './DrinkCard';
+import { DrinkCard } from './DrinkCard';
 
 const mockDrink = {
   idDrink: '1',
